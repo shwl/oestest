30bac9abb4809adc196f1d120ba35f2e4b280b02
swl <swl@timevale.com>
2018-02-09 14:08:53 +0800

    有输出参数的函数根据返回值重新申请缓存区，再次调用

--- GenTestCase.py
+++ GenTestCase.py
@@ -53,7 +53,8 @@ testCase.write("\n")
 nrows = sheet0.nrows    #获取行总数
 print("sheet_nrows:", nrows)
 rowindex = 4
-funcendstr = ")\nprint(\"res:\", res)\n\n"
+funcendstr = ")\n"
+printres = "print(funcname + \"res:\", res)\n\n"
 param_null = "param_%d = None\n"
 param_pu_char = "param_%d = ctypes.create_string_buffer(%s)\n"
 param_pu_char_in = "param_%d = r\"%s\"\n"
@@ -64,11 +65,12 @@ param_u_long = "param_%d = c_ulong(%s)\n"
 for i in range(rowindex, nrows):
     colindex = 0
     funcname = sheet0.cell_value(i, colindex)
-    defaultres = sheet0.cell_value(i, colindex + 1)
     if(0 == len(funcname)):
         continue
     else:
-        print("funcname:%s\ndefaultres:%s\n"%(funcname, defaultres))
+        defaultres = int(sheet0.cell_value(i, colindex + 1))
+        testCase.write("defaultres = %d\nfuncname = '%s'\n"%(defaultres, funcname));
+        print("funcname:%s\ndefaultres:%d\n"%(funcname, defaultres))
         #参数初始化
         for j in range(i, nrows):
             funcname1 = sheet0.cell_value(j, colindex)
@@ -105,7 +107,7 @@ for i in range(rowindex, nrows):
             if(k != i):
                 testCase.write(", ")
             testCase.write("param_%d"%(k - i))
-        testCase.write(funcendstr)
+        testCase.write(funcendstr + printres)
         
         #打印输出参数
         for j in range(i, nrows):
@@ -117,7 +119,10 @@ for i in range(rowindex, nrows):
                 if("out" in param_att):
                     if("unsigned char*" == param_type):
                         testCase.write("if(param_%d != None):\n"%(j - i))
-                        testCase.write("\tbuf = param_%d.value.decode('utf-8')\n"%(j - i))
+                        testCase.write("\ttry:\n");
+                        testCase.write("\t\tbuf = param_%d.raw[0:int(param_%d[0])].decode('utf-8')\n"%(j - i, j - i + 1))
+                        testCase.write("\texcept UnicodeDecodeError:\n");
+                        testCase.write("\t\tbuf = ''.join(['%%02x' %% b for b in param_%d.raw[0:int(param_%d[0])]])\n"%(j - i, j - i + 1))
                         testCase.write("\tprint(\"%s:%%s\"%%(buf))\n"%(param_name))
                         testCase.write("else:\n")
                         testCase.write("\tprint(\"%s:None\")\n"%(param_name))
@@ -129,5 +134,52 @@ for i in range(rowindex, nrows):
         testCase.write("\n")
 
         #输出参数根据返回值重新申请缓存区
-
+        print("funcname:%s\ndefaultres:%s\n"%(funcname, defaultres))
+        testCase.write("if(res == defaultres):\n");
+        #参数初始化
+        for j in range(i, nrows):
+            funcname1 = sheet0.cell_value(j, colindex)
+            if(j == i or 0 == len(funcname1)):
+                param_att = sheet0.cell_value(j, colindex + 2)
+                param_type = sheet0.cell_value(j, colindex + 3)
+                param_value = sheet0.cell_value(j, colindex + 4)
+                #print("param_att:%s\nparam_type:%s\nparam_value:%s\n"%(param_att, param_type, param_value))
+                if("unsigned char*" == param_type):
+                    if("out" == param_att):
+                        testCase.write("\t" + param_pu_char%(j - i, "param_%d[0]"%(j - i + 1)))
+            else:
+                break
+        #函数调用
+        testCase.write("\tres = %s.%s("%(tgdll, funcname))
+        if(j + 1 == nrows):
+            j = j + 1
+        for k in range(i, j):
+            if(k != i):
+                testCase.write(", ")
+            testCase.write("param_%d"%(k - i))
+        testCase.write(funcendstr + "\t" + printres)
+        
+        #打印输出参数
+        for j in range(i, nrows):
+            funcname1 = sheet0.cell_value(j, colindex)
+            if(j == i or 0 == len(funcname1)):
+                param_att = sheet0.cell_value(j, colindex + 2)
+                param_type = sheet0.cell_value(j, colindex + 3)
+                param_name = sheet0.cell_value(j, colindex + 5)
+                if("out" in param_att):
+                    if("unsigned char*" == param_type):
+                        testCase.write("\tif(param_%d != None):\n"%(j - i))
+                        testCase.write("\t\ttry:\n");
+                        testCase.write("\t\t\tbuf = param_%d.raw[0:int(param_%d[0])].decode('utf-8')\n"%(j - i, j - i + 1))
+                        testCase.write("\t\texcept UnicodeDecodeError:\n");
+                        testCase.write("\t\t\tbuf = ''.join(['%%02x' %% b for b in param_%d.raw[0:int(param_%d[0])]])\n"%(j - i, j - i + 1))
+                        testCase.write("\t\tprint(\"%s:%%s\"%%(buf))\n"%(param_name))
+                        testCase.write("\telse:\n")
+                        testCase.write("\t\tprint(\"%s:None\")\n"%(param_name))
+                    elif("int*" == param_type):
+                        testCase.write("\tprint(\"%s:%%s\"%%(param_%d[0]))\n"%(param_name, j - i))
+            else:
+                break
+        testCase.write("\tprint('\\n')")
+        testCase.write("\t\n")
 testCase.close()
